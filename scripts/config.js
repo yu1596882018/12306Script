8588826136fba529e801302628ac2686573e4101
wangxinyu <1596882018@qq.com>
2020-01-07 12:32:52 +0800

    修改配置

--- scripts/config.js
+++ scripts/config.js
@@ -1,10 +1,10 @@
 const redis = require('redis');
-const redisDb = redis.createClient(6379, 'localhost');
-// const redisDb = redis.createClient(6379, '39.108.161.237');
+const localConfig = require('./localConfig');
+const redisDb = redis.createClient(6379, localConfig.redisHost);
 let userCookie = 'JSESSIONID=9032181F66FBA8B98C78B038B2FFC61D; tk=bzUFTKw-tnQ12SxSSH7z4i6qdlecMqE9AA6tVxjekk0xhy1y0; _jc_save_wfdc_flag=dc; BIGipServerotn=535298314.50210.0000; RAIL_EXPIRATION=1578413544697; RAIL_DEVICEID=IpwYVKNMh6Tizi15NP58IE-ZKG7UzuFqxsvybQG0VQ5h3ni6cPbjVWjQk2o2wuTWDhgbPx0-DgveztEB7XX1X0NY6sYbUBNy9bSddRWJbBfYzRUAsVmdXyS-UGJcaFdmdCWHwieKKQqJ3VEiSMTPpj7hNXBM_CP3; BIGipServerpassport=921174282.50215.0000; route=c5c62a339e7744272a54643b3be5bf64; _jc_save_toDate=2020-01-04; _jc_save_fromStation=%u90B5%u9633%2CSYQ; _jc_save_toStation=%u6DF1%u5733%u5317%2CIOQ; _jc_save_fromDate=2020-01-04';
 
 redisDb.get('userCookie', function (err, v) {
-    // v && (userCookie = v);
+    v && (userCookie = v);
 });
 
 module.exports = {
--- scripts/loopCheckUser.js
+++ scripts/loopCheckUser.js
@@ -2,7 +2,7 @@
 const superagent = require('superagent');
 const setHeaders = require('./setHeaders');
 const getCodeImage = require('./getCodeImage');
-
+/*
 const start = async () => {
     let checkUserResult = await setHeaders(superagent.post('https://kyfw.12306.cn/otn/login/checkUser'))
         .send({
@@ -16,6 +16,25 @@ const start = async () => {
             sendmail: true
         });
     }
+}*/
+
+const start = async () => {
+    let chechFaceResult = await setHeaders(superagent.post('https://kyfw.12306.cn/otn/afterNate/chechFace'))
+        .send({
+            secretList: `q0ayjkIIX64yKjLwF6K7dCIDp25C4iVs5L3m2ybXBNNyxW%2FFjsLU29yawLt2XGdqYZQv%2F9G7R1fA%0AbPUUb1BOHw7PZgJ87sUI3PLmKIRLhXkAoaWejL4IF45A%2Be7TrF4pQOZaUB4Bu5Xvm4Bnr6%2BZz95M%0A7%2FHKPaViGh0Jm1J5Nkzq0mwRgKAAMwfweEmLuHlanN2JsGMahUqEkFJcXB5MNhYwuTV95WNdNXjx%0ASIS2do31TfB2CKDBVAzgNF6%2FzIwyuYIY06LzNKPvnRgEt%2BFTdMhYfsleQDOZI2Hw64YWwhyhoV2p%0A#9|`,
+            _json_att: ''
+        });
+    // console.log('checkUserResult', checkUserResult.text);
+    // let checkUserData = JSON.parse(checkUserResult.text);
+    let checkUserData = chechFaceResult.body;
+    console.log(chechFaceResult.body)
+    if (!checkUserData.data.login_flag) {
+        console.log('cookie失效');
+        getCodeImage({
+            sendmail: true
+        });
+    }
 }
+
 setTimeout(start, 3000);
 setInterval(start, 10 * 60 * 1000);
