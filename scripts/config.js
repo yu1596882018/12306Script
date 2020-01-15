cb7d2f7088bb08033a9161a0be324cd20dca7caa
wangxinyu <1596882018@qq.com>
2020-01-15 10:30:50 +0800

    更新cookie

--- scripts/config.js
+++ scripts/config.js
@@ -3,10 +3,10 @@ const localConfig = require('./localConfig');
 const redisDb = redis.createClient(6379, localConfig.redisHost);
 const redisPub = redis.createClient(6379, localConfig.redisHost);
 const redisSub = redis.createClient(6379, localConfig.redisHost);
-let userCookie = 'JSESSIONID=68DE645B3FC048D4E9346D8EB10546D7; tk=rkXOL2t0qTOB8b_S5_N21f95MlvNwh6TFT9U30FJJssnxy1y0; _jc_save_wfdc_flag=dc; _jc_save_fromStation=%u6DF1%u5733%u5317%2CIOQ; _jc_save_toStation=%u9686%u56DE%2CLHA; BIGipServerotn=1978138890.50210.0000; BIGipServerpassport=837288202.50215.0000; RAIL_EXPIRATION=1579043915275; RAIL_DEVICEID=GSTWBXJRfxXI-pQb4HBLATGmYDPxbQ5lWEOO3WAle9DuKOJlMw06KPwfZxJWebSmfa5U8i-gk-TQ_yOw6_Zop_KRY7JcOWEn2EUaew0mn22F6JKiTizDyjkEneF3EBOn9YsvXtzvNInXy-kSjLFU7CyDL0rFMNqI; route=9036359bb8a8a461c164a04f8f50b252; _jc_save_toDate=2020-01-11; _jc_save_fromDate=2020-02-03';
+let userCookie = 'JSESSIONID=5E5936D12892AE34A32A9FF4C9CB0645; tk=HAz9Iz5YURgl7R6QygTfTvsJR8J-ki8uLaM6IIC4rBk73y1y0; RAIL_EXPIRATION=1579177283490; RAIL_DEVICEID=K0WBD1Ahww1yJe394XZr9L4AMiUev5hYfJdj5zVA3l5LFK5YAgi3mcsmSgFI-TY3jM9f6PNScxVVjGBzM6fsyqF9qgw4cQaTgxoII4xv-iRNekWz_zeZoqcVL0SQSgVwoIQJZzH4CcoFN8d67L_oNBPqYFHloNFI; _jc_save_wfdc_flag=dc; _jc_save_fromStation=%u6606%u660E%2CKMM; _jc_save_toStation=%u6DF1%u5733%2CSZQ; BIGipServerpassport=921174282.50215.0000; route=495c805987d0f5c8c84b14f60212447d; _jc_save_fromDate=2020-01-15; _jc_save_toDate=2020-01-15; BIGipServerotn=837812746.38945.0000; BIGipServerpool_passport=166527498.50215.0000';
 
 redisDb.get('userCookie', function (err, v) {
-    v && (userCookie = v);
+    // v && (userCookie = v);
 });
 
 module.exports = {
